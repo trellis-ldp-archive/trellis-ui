dda9263d82fbc6b1367dafa6a41f619811309ae3
Aaron Coburn <acoburn@apache.org>
2018-05-18 12:26:15 -0400

    Minor cleanup

--- src/index.js
+++ src/index.js
@@ -2,25 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux'
-import { reducer as formReducer } from 'redux-form'
+import { reducer as formReducer, actionTypes } from 'redux-form'
 import './assets/index.css';
 import LdpApp from './components/LdpApp';
 import registerServiceWorker from './registerServiceWorker';
 
-// The submit action type
-const SUBMITTED = "@@redux-form/SET_SUBMIT_SUCCEEDED";
-
 const store = createStore(combineReducers({
   form: formReducer.plugin({
-    resource: (state, action) => {
-      switch(action.type) {
-        // Reset the form
-        case SUBMITTED:
-          return undefined;
-        default:
-          return state
-      }
-    }
+    // Reset the form upon submission
+    resource: (state, action) => action.type === actionTypes.SET_SUBMIT_SUCCEEDED ? undefined : state
   })
 }));
 
